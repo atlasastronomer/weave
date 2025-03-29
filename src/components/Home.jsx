a7361225f23928b7d8b94a69ba5438f8bc0da85f
Atlas Astronomer <atlasastronomer@gmail.com>
2025-03-28 20:19:29 -0400

    Added background and pfp functionality

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
   const [fileUrl, setFileUrl] = useState(null)
   const [bgUrl, setBgUrl] = useState(null)
   const profilePicRef = useRef(null)
+  const backgroundImageRef = useRef(null)
 
   const changeProfilePic = (e) => {
     e.preventDefault()
@@ -15,7 +16,20 @@ const Home = () => {
       setFileUrl(url)
     }
     else {
-      console.log('No file selected')
+      alert('No file selected')
+    }
+  }
+
+  const changeBackground = (e) => {
+    e.preventDefault()
+
+    const file = backgroundImageRef.current.files[0]
+    if (file) {
+      const url = URL.createObjectURL(file)
+      setBgUrl(url)
+    }
+    else {
+      alert('No file selected')
     }
   }
 
@@ -29,12 +43,18 @@ const Home = () => {
         <input type='file' ref={profilePicRef} accept='image/png, image/jpg, image/jpeg'/>
         <button type='submit'>Set profile picture</button>
       </form>
-      <form onSubmit={postLink}>
 
+      <form onSubmit={changeBackground}>
+        <input type='file' ref={backgroundImageRef} accept='image/png, image/jpg, image/jpeg'/>
+        <button type='submit'>Set background</button>
       </form>
       
-      <div className="upper-container" style={{backgroundImage: 'url(/assets/default-bg.jpg)'}}>
+      <div className='upper-container' style={{backgroundImage: bgUrl ? `url(${bgUrl})` : 'url(/assets/default-bg.jpg)'}}>
         <img src={fileUrl || '/assets/default-avatar.jpg'} className='profile-picture'/>
+      </div>
+
+      <div className='lower-container'>
+
       </div>
     </>
   )
