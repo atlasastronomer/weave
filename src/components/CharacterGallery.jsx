c56a88b9ba469b3ffbc1eb1ccc49b2212787ab27
Atlas Astronomer <atlasastronomer@gmail.com>
2025-04-20 14:01:35 -0400

    update

--- src/components/CharacterGallery.jsx
+++ src/components/CharacterGallery.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { Image } from 'cloudinary-react'
 import axios from 'axios'
 import axiosService from '/src/services/axios'
 
