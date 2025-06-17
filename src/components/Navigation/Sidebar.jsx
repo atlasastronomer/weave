950d18eb728e687cbe084347c2817b8b3d2e1b9c
Atlas Astronomer <atlasastronomer@gmail.com>
2025-06-17 19:27:59 -0400

    Added a search bar to the sidebar component. Adding search functionality next

--- src/components/Navigation/Sidebar.jsx
+++ src/components/Navigation/Sidebar.jsx
@@ -1,9 +1,40 @@
-import './Sidebar.css'
 import { useState, useEffect } from 'react'
+import userService from '/src/services/userService'
+import './Sidebar.css'
 
 const SideBar = () => {
+  const [token, setToken] = useState('')
+
+  const [searchValue, setSearchValue] = useState('')
+  const [users, setUsers] = useState([])
+  const [visibleUsers, setVisibleUsers] = useState([])
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem('token')
+
+    setToken(storedToken)
+
+    if(storedToken) {
+      loadUsers()
+    }
+  }, [])
+
+  const loadUsers = async () => {
+      const res = await userService.getAllUsers()
+      setUsers(res.data)
+  }
+
   return (
     <div className='side-bar'>
+      <div className='search-bar'>
+        <i className="fa-solid fa-search fa-lg fa-icon"></i>
+        <input
+          className='search-field'
+          placeholder='Search Weave'
+          value={searchValue}
+          onChange = {e => setSearchValue(e.target.value)}
+        />
+      </div>
     </div>
   )
 }
