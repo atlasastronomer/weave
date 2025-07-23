8ebd2606811e17698afcdcbedb9ad75318a4554f
Atlas Astronomer <atlasastronomer@gmail.com>
2025-07-23 18:11:55 -0400

    Frontend displays messaging between users. Will add message box so users can send each other messages thorugh the frontend.

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -3,43 +3,62 @@ import { useNavigate, useLocation, useParams } from 'react-router-dom'
 import { SearchEntry } from './SearchEntry'
 import { Avatar } from '../Home/Avatar'
 import userService from '/src/services/userService'
+import messagesService from '../../services/messagesService'
 import './MessagesSearchbar.css'
 import './MessagesConversation.css'
 
 const Messages = () => {
   const [token, setToken] = useState('')
-    
+
   const [searchValue, setSearchValue] = useState('')
   const [users, setUsers] = useState([])
   const [filteredUsers, setFilteredUsers] = useState([])
   
+  const [senderID, setSenderID] = useState()
+  
   const { username } = useParams()
-  const [userName, setUserName] = useState()
-  const [userUsername, setUserUsername] = useState()
-  const [userAbout, setUserAbout] = useState()
-  const [userAvatar, setUserAvatar] = useState()
+  const [recipientName, setRecipientName] = useState()
+  const [recipientUsername, setRecipientUsername] = useState()
+  const [recipientAbout, setRecipientAbout] = useState()
+  const [recipientAvatar, setRecipientAvatar] = useState()
+  const [messages, setMessages] = useState([])
 
   const navigate = useNavigate()
   const location = useLocation()
 
   useEffect(() => {
+  console.log('senderID:', senderID)
+}, [senderID])
+
+  useEffect(() => {
     const storedToken = localStorage.getItem('token')
-    setToken(storedToken)
-    userService.setToken(storedToken)
+    if (!storedToken) return
 
-    if (storedToken) {
-      loadUsers()
+    const getToken = () => {
+      setToken(storedToken)
+      userService.setToken(storedToken)
+      messagesService.setToken(storedToken)
     }
+
+    const getID = () => {
+      const payload = JSON.parse(atob(storedToken.split('.')[1]))
+      const userID = payload.id
+      setSenderID(userID)
+    }
+
+    const loadUsers = async () => {
+      try {
+        const users = await userService.getAllUsers()
+        setUsers(users)
+      } catch (error) {
+        console.error('Failed to load users:', error)
+      }
+    }
+
+    getToken()
+    getID()
+    loadUsers()
   }, [])
-
-  const loadUsers = async () => {
-    try {
-      const users = await userService.getAllUsers()
-      setUsers(users)
-    } catch (error) {
-      console.error('Failed to load users:', error)
-    }
-  }
 
   useEffect(() => {
     setSearchValue('')
@@ -52,70 +71,100 @@ const Messages = () => {
       return
     }
 
-    const filtered = users.filter((user) =>
+    const filtered = users.filter(user =>
       user.username.toLowerCase().includes(searchValue.toLowerCase())
     )
     setFilteredUsers(filtered)
   }, [searchValue, users])
 
   useEffect(() => {
-    const fetchUser = async () => {
-      if (!username) return
+    if (!token || !username) return
 
+    const fetchData = async () => {
       try {
         const user = await userService.getUser(username)
-        setUserAvatar(user.avatar)
-        setUserName(user.name)
-        setUserUsername(user.username)
-        setUserAbout(user.about?.about || 'Hello! Welcome to my space.')
-      } catch (error) { 
+        setRecipientAvatar(user.avatar)
+        setRecipientName(user.name)
+        setRecipientUsername(user.username)
+        setRecipientAbout(user.about?.about || 'Hello! Welcome to my space.')
+      } catch (error) {
         console.error('Error fetching user:', error)
+      }
+
+      try {
+        const fetchedMessages = await messagesService.getMessagesWith(username)
+        setMessages(fetchedMessages)
+      } catch (error) {
+        console.error('Error fetching messages:', error)
       }
     }
 
-    fetchUser()
-  }, [username])
+    fetchData()
+  }, [token, username])
 
   const openConversation = (username) => {
     navigate(`/messages/${username}`)
   }
+
   return (
-  <div className='messages-page-wrapper'>
-    <div className='messages-search-wrapper'>
-      <p className='messages-search-title'>Messages</p>
-      <div className='messages-search-bar'>
-        <i className="fa-solid fa-search fa-lg fa-icon"></i>
-        <input
-          className='messages-search-field'
-          placeholder='Search Direct Messages'
-          value={searchValue}
-          onChange={e => setSearchValue(e.target.value)}
-        />
+    <div className='messages-page-wrapper'>
+      <div className='messages-search-wrapper'>
+        <p className='messages-search-title'>Messages</p>
+        <div className='messages-search-bar'>
+          <i className="fa-solid fa-search fa-lg fa-icon"></i>
+          <input
+            className='messages-search-field'
+            placeholder='Search Direct Messages'
+            value={searchValue}
+            onChange={e => setSearchValue(e.target.value)}
+          />
+        </div>
+        {filteredUsers.length !== 0 && (
+          <div className='messages-search-results'>
+            {filteredUsers.map((user) => 
+              <SearchEntry user={user} key={user.id} openConversation={() => openConversation(user.username)} />
+            )}
+          </div>
+        )}
       </div>
-      {filteredUsers.length !== 0 && (
-        <div className='messages-search-results'>
-          {filteredUsers.map((user) => 
-            <SearchEntry user={user} key={user.id} openConversation={() => openConversation(user.username)}/>
-          )}
-        </div>
-      )}
+
+      <div className='messages-conversation-wrapper'>
+        {username ? (
+          <>
+            <div className='messages-conversation-header' onClick={() => { navigate(`/${recipientUsername}`) }}>
+              <Avatar avatar={recipientAvatar} classname={'messages-conversation-header-avatar'} />
+              <p className='messages-conversation-title'>{recipientName}</p>
+              <p className='messages-conversation-username'>@{recipientUsername}</p>
+              <p className='messages-conversation-about'>{recipientAbout}</p>
+            </div>
+            <div className='messages-conversation-body'>
+              {messages.map(msg => (
+                <div key={msg.id}>
+                  {(msg.sender === senderID ?
+                    <div className='sender-message-container'>
+                      <div className='sender-message'>
+                        {msg.content}
+                      </div>
+                    </div>
+                    :
+                    <div className='recipient-message-container'>
+                      <div className='recipient-message'>
+                        {msg.content}
+                      </div>
+                    </div>)
+                  }
+                </div>
+              ))}
+            </div>
+          </>
+        ) : (
+          <div>
+            <p className='messages-conversation-no_selection-title'>Select a message</p>
+            <p className='messages-conversation-no_selection-text'>No thread picked yet. Weave a new conversation or select one to start spinning.</p>
+          </div>
+        )}
+      </div>
     </div>
-    <div className='messages-conversation-wrapper'>
-      {username ?
-        <div className='messages-conversation-header' onClick={() => {navigate(`/${userUsername}`)}}>
-          <Avatar avatar={userAvatar} classname={'messages-conversation-avatar'}/>
-          <p className='messages-conversation-title'>{userName}</p>
-          <p className='messages-conversation-username'>@{userUsername}</p>
-          <p className='messages-conversation-about'>{userAbout}</p>
-        </div>
-        :
-        <div>
-          <p className='messages-conversation-no_selection-title'>Select a message</p>
-          <p className='messages-conversation-no_selection-text'>No thread picked yet. Weave a new conversation or select one to start spinning.</p>
-        </div>  
-      }
-    </div>
-  </div>
   )
 }
 
---
+++ src/services/messagesService.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+const VITE_PORT = import.meta.env.VITE_PORT || 3001
+const baseUrl = `http://localhost:${VITE_PORT}/api/messages`
+
+let token = null
+
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`
+}
+
+const getMessagesWith = async (username) => {
+  const config = {
+    headers: {Authorization: token},
+  }
+
+  const res = await axios.get(`${baseUrl}/${username}`, config)
+  return res.data
+}
+
+const sendMessage = async (username, newObject) => {
+  const config = {
+    headers: {Authorization: token},
+  }
+
+  const res = await axios.post(`${baseUrl}/${username}`, newObject, config)
+  return res.data
+}
+
+export default { setToken, getMessagesWith, sendMessage }
